3a9f6a46ab0236c63b44cb495bf787b82c99449f
MB <marine.bony@outlook.fr>
2024-06-17 11:12:28 +0200

    encoding finis

--- code.py
+++ code.py
@@ -8,24 +8,26 @@ data = pd.read_csv('Data_Arbre.csv')
 
 
 #1) on sélectionne nos colonnes utiles :
-haut_tot = data["haut_tot"]
-haut_tronc = data["haut_tronc"]
-diam_tronc = data["tronc_diam"]
-age_estim = data["age_estim"]
+haut_tot = data[["haut_tot"]]
+haut_tronc = data[["haut_tronc"]]
+diam_tronc = data[["tronc_diam"]]
+age_estim = data[["age_estim"]]
 
-#on les mets dans un data frame :
-new_data = {"haut_tot" : data["haut_tot"], "haut_tronc" : data["haut_tronc"], "diam_tronc" : data["tronc_diam"], "age_estim" : data["age_estim"]}
-#print(new_data)
 
 #2) on encode nos données :
 encoding = OrdinalEncoder()
-enc_haut_tot = encoding.fit_transform(new_data["haut_tot"])
-enc_haut_tronc = encoding.fit_transform(new_data["haut_tronc"])
-enc_diam_tronc = encoding.fit_transform(new_data["diam_tronc"])
-enc_age_estim = encoding.fit_transform(new_data["age_estim"])
-print(enc_age_estim)
-# print(enc_haut_tronc)
+enc_haut_tot = encoding.fit_transform(haut_tot)
+enc_haut_tronc = encoding.fit_transform(haut_tronc)
+enc_diam_tronc = encoding.fit_transform(diam_tronc)
+enc_age_estim = encoding.fit_transform(age_estim)
+
+# print(enc_age_estim)
+print(enc_haut_tronc)
 # print(enc_haut_tot)
 # print(enc_diam_tronc)
+
+#on les mets dans un data frame :
+# new_data = {"haut_tot" : data[["haut_tot"]], "haut_tronc" : data[["haut_tronc"]], "diam_tronc" : data[["tronc_diam"]], "age_estim" : data[["age_estim"]]}
+#print(new_data)
 
 
